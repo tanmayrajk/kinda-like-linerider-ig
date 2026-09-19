4012a504b8dbb1b58b755bb81bab163bcd71c718
istoocold <iksha.hackclub@gmail.com>
2026-09-19 23:29:15 +0530

    debug

--- scripts/pen.gd
+++ scripts/pen.gd
@@ -16,22 +16,25 @@ func _process(delta):
 		click_offset = get_global_mouse_position() - cam.global_position
 		last_cell_x = local_to_map(to_local(cam.global_position + click_offset)).x
 		last_spawn_x = get_global_mouse_position().x
+		
 		click_world_y = get_global_mouse_position().y
 		set_cell(local_to_map(to_local(get_global_mouse_position())),0,atlas_coords)
 	if Input.is_action_just_released("click"):
 		is_drawing = false
 		
+		
 	if not is_drawing:
-		return
+		return 
 		
 	var cell_fill = local_to_map(to_local(cam.global_position + click_offset))
 	var paint_x = cam.global_position.x + click_offset.x
 	while paint_x - last_spawn_x >= spawn_distance:
+		
 		last_spawn_x +=  spawn_distance
 		var spawn_cell = local_to_map(to_local(Vector2(last_spawn_x, click_world_y)))
 		set_cell(spawn_cell,0,atlas_coords)
 		
 	
-		
 	last_cell_x = cell_fill.x
+	
 		
