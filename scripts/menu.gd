de426fb0596c85c3584108ec00184c0d217e3ddd
istoocold <iksha.hackclub@gmail.com>
2026-09-20 18:25:07 +0530

    added menu scnee tih button for various purpose and code to jsu

---
+++ scripts/menu.gd
@@ -0,0 +1,11 @@
+extends Control
+var play_scene = load()
+
+
+
+
+@onready var play_button = $PlayButton
+@onready var garage_button = $GarageButton
+@onready var pens_button = $Pens_Button
+@onready var setting_button = $SettingButton
+@onready var levels_button = $LevelButton
